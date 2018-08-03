91bebc2158166f3c3b29e855638fc0429d748aff
ruuuh20 <pkim2320@gmail.com>
2018-08-03 00:03:38 -0400

    adding logout

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -15,3 +15,4 @@ export const FETCH_REGISTERED_FAIL = 'FETCH_REGISTERED_FAIL';
 export const AUTH_START = 'AUTH_START'
 export const AUTH_SUCCESS = 'AUTH_SUCCESS'
 export const AUTH_FAIL = 'AUTH_FAIL'
+export const AUTH_LOGOUT = 'AUTH_LOGOUT'
